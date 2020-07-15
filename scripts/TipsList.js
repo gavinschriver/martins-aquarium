ff4701fdcbc70097c6448548c6ccbf1845806125
Gavin Schriver <alexanderschriver@gmail.com>
2020-07-15 09:23:56 -0500

    added tips module

--- scripts/TipsList.js
+++ scripts/TipsList.js
@@ -1,17 +1,19 @@
-import {makeCopyOfTipsCollection} from "./LocationsDataProvider.js"
+import {makeCopyOfTipsCollection} from "./TipsDataProvider.js"
 import {makeAnHTMLTip} from "./tipsFactory.js"
 
 export const listOfTips = () => {
     const whereToPutTipsinHTML = document.querySelector(".content--right")
 
-    const tipArrayCopy = makeCopyOfTipsCollection()
+    const tipsArrayCopy = makeCopyOfTipsCollection()
 
     let allTipsAsHTML = ""
-    for (const currentTipObj of tipArrayCopy) {
-        allTipsAsHTML += `
-        <aside class="tips">  <!--creates parent aside element -->
-        ${allTipsAsHTML}
-        <aside>
-        `
+    for (const currentTipObj of tipsArrayCopy) {
+        allTipsAsHTML += makeAnHTMLTip(currentTipObj)
     }
+
+    whereToPutTipsinHTML.innerHTML += `
+        <aside class="tips"> 
+        ${allTipsAsHTML}
+        </aside>
+    ` 
 }
